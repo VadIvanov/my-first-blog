960b9d8af9e3b59cab6ad82ebb68be5815acd527
Vadim <viavna@mail.ru>
2020-04-16 22:50:16 +0300

    Changed the HTML for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
+#@admin.register(Post)
+#class PostAdmin(admin.ModelAdmin):
+    #list_display = ('title',  'author', 'published_date')
+    #list_filter = ('created_date', 'published_date', 'author')
+    #search_fields = ('title', 'text')
+    #date_hierarchy = 'published_date'
